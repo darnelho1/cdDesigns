6d62a5cdf59082921af8af667eeea4de524bfede
BeroMaze <Berning.corey@gmail.com>
2016-07-07 13:09:01 -0700

    add portfolio info

---
+++ db.js
@@ -0,0 +1,47 @@
+var articles = [
+    {
+      id:                 'm1tree',
+      title:              'M1 Tree & Landscape',
+      publishedOn:        'June 25, 2016',
+      url:                'm1tree',
+      img1:                '/images/website/m1Home.png',
+      img2:                '/images/website/m1HomeMobile.png',
+      img3:                '/images/website/m1HomeHover.png',
+      img4:                '/images/website/m1Contact.png',
+      img5:                '/images/website/m1Quote.png',
+      img6:                '/images/website/m1city.png',
+      img7:                '/images/website/m1CityMobile.png',
+      link:               'http://www.m1tree.com',
+      desc:               'This site was created for a Tree and landscaping company call M1. The goal for this site was to have easy usability for customers to be able to get ahold of the company with any questions or to request work to be done. There is also a page for users to locate a list of their city ordinances.'
+    },
+    {
+      id:                 'happyHour',
+      title:              'Seattle Happy Hour App',
+      publishedOn:        'draft',
+      url:                'happyhour',
+      img1:                '/images/website/happyHourHome.png',
+      img2:                '/images/website/happyHourSearch.png',
+      img3:                '/images/website/happyHourMap.png',
+      img4:                '/images/website/happyHourMapChoose.png',
+      img5:                '/images/website/happyHourLocation.png',
+      img6:                '/images/website/happyHourMapChoose.png',
+      img7:                '/images/website/happyHourServer.png',
+      link:               'http://happyhourapp.herokuapp.com',
+      desc:               'Seattle Happy Hour App is a mobile and desktop ready application to help people find happy hour specials in Seattle. Using the Yelp and Google API the user can search by; term, neighborhood, and more. The site uses the user’s location and search criteria to find the closest restaurant that has happy hour. By using different JS libraries the site was designed so that the user would be able to see the times of the happy hour and the locations that have happy hours going on currently.'
+    },
+    {
+      id:                 'whalephants',
+      title:              'Whalephants Final Project',
+      publishedOn:        '12/5/2015',
+      url:                'whalephants',
+      img1:                '/images/website/whalephantsHomePage.png',
+      img2:                '/images/website/whalephantsAbout.png',
+      img3:                '/images/website/whalephantsVote.png',
+      img4:                '/images/website/whalephantsCount.png',
+      img5:                '/images/website/whalephantsContact.png',
+      img6:                '/images/website/whalephantsHangman.png',
+      img7:                '/images/website/whalephantsThanks.png',
+      link:                'http://whalephants.herokuapp.com',
+      desc:               'Whalephants is a project that my group worked on. We built a website for an organization that builds a camp out at Burning Man each year. This website is for people to come check out what the camp is about and what all they bring to the community. There are many different pages; about page, drink voting page, countdown page, game page, and a thank you page. Head over to the Whalephants website and vote on the next drinks that they will be making out at Burning Man or go over and play the Hang Man game. There are three words, see if you can guess all of them. </br>'
+    }
+  ];
--- events.js
+++ events.js
@@ -1,8 +1,5 @@
-
 $('.navLink').click(function(){
   console.log(this.text);
-  $("#"+this.text).show();
-  $("#"+this.text).css('display', 'flex');
   if (pageOn === 'HOME') {
     $('.landingPageSection').hide();
     $('#mainSection').css('background-image', 'url("./images/servicesBackground.jpg")');
@@ -15,10 +12,42 @@ $('.navLink').click(function(){
     $('#'+pageOn).hide();
     pageOn = this.text;
   }
-
+  $("#"+this.text).show();
+  $("#"+this.text).css('display', 'flex');
   $('#pageNav').hide();
   $('#pageNavHeader').css('display', 'flex');
-
+  if (pageOn === 'PORTFOLIO') {
+    $('#scrollingArticles').empty();
+    articles.forEach(function(each) {
+      var template = $('#articleTemplate').html();
+      var compileTemplate = Handlebars.compile(template);
+      var html = compileTemplate(each);
+      $('#scrollingArticles').append(html);
+      var galleryTop = new Swiper('#'+each.id+' .gallery-top', {
+        effect: 'cube',
+        grabCursor: true,
+        cube: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94
+        },
+          nextButton: '#'+each.id+' .swiper-button-next',
+          prevButton: '#'+each.id+' .swiper-button-prev',
+          spaceBetween: 0,
+          keyboardControl: true
+      });
+    });
+    var swiper = new Swiper('#PORTFOLIO', {
+        pagination: '.swiper-pagination',
+        direction: 'vertical',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+        keyboardControl: true,
+        mousewheelControl: true
+    });
+  }
 });
 
 $('#pageTitle').click(function(event) {
@@ -59,4 +88,59 @@ $('#submitForm').on('click', function(event) {
   else {
     alert('Please fill out form completely.');
   }
+});
+
+$('.lpScrollingImage').on('click', function(event) {
+  event.preventDefault();
+  var imageClick = $(this).attr('title');
+  console.log(imageClick);
+  $('.landingPageSection').hide();
+  $('#mainSection').css('background-image', 'url("./images/servicesBackground.jpg")');
+  $('#mainSection').removeClass('homePageBackground');
+  $('#mainSection').addClass('otherBackground');
+  clearInterval(changeImageTiming);
+  pageOn = 'PORTFOLIO';
+  $("#PORTFOLIO").show();
+  $("#PORTFOLIO").css('display', 'flex');
+  $('#pageNav').hide();
+  $('#pageNavHeader').css('display', 'flex');
+  $('#scrollingArticles').empty();
+  articles.forEach(function(each) {
+    var template = $('#articleTemplate').html();
+    var compileTemplate = Handlebars.compile(template);
+    var html = compileTemplate(each);
+    $('#scrollingArticles').append(html);
+    var galleryTop = new Swiper('#'+each.id+' .gallery-top', {
+      effect: 'cube',
+      grabCursor: true,
+      cube: {
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94
+      },
+        nextButton: '#'+each.id+' .swiper-button-next',
+        prevButton: '#'+each.id+' .swiper-button-prev',
+        spaceBetween: 0,
+        keyboardControl: true
+    });
+  });
+  var swiper = new Swiper('#PORTFOLIO', {
+      pagination: '.swiper-pagination',
+      direction: 'vertical',
+      slidesPerView: 1,
+      paginationClickable: true,
+      spaceBetween: 0,
+      keyboardControl: true,
+      mousewheelControl: true
+  });
+  var eachCounter = -1;
+  var pickedAtricle = 0;
+  articles.forEach(function(each) {
+    eachCounter ++;
+    if (each.id === imageClick) {
+      pickedAtricle = eachCounter;
+      swiper.slideTo(pickedAtricle);
+    }
+  });
 });
